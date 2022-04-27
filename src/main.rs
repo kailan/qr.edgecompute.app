ad48aa6c4456bc493d962982650b4501eedeaf96
Kailan Blanks <kblanks@fastly.com>
2022-04-27 13:27:59 +0100

    Stream PNG to client as it is encoded

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,67 @@
 use fastly::http::{Method, StatusCode};
-use fastly::{Error, mime, Request, Response};
-use image::{ColorType, Luma};
+use fastly::{mime, Error, Request, Response};
 use image::png::PngEncoder;
+use image::{ColorType, Luma};
 use qrcode::QrCode;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
 struct CodeParams {
-    url: Option<String>
+    url: Option<String>,
 }
 
-#[fastly::main]
-fn main(req: Request) -> Result<Response, Error> {
+fn handle_request(req: Request) -> Result<(), Error> {
     match (req.get_method(), req.get_path()) {
         (&Method::GET, "/qr.png") => {
+            // Fetch target URL from query string
             let params: CodeParams = req.get_query()?;
+
             if let Some(url) = params.url {
-                if url.len() > 64 {
-                    return Ok(Response::from_status(StatusCode::BAD_REQUEST).with_body_text_plain("URL too long"));
+                // Limit the content length
+                if url.len() > 128 {
+                    Response::from_status(StatusCode::BAD_REQUEST)
+                        .with_body_text_plain("URL too long")
+                        .stream_to_client();
+                    return Ok(());
                 }
 
+                // Create a QR code
                 let code = QrCode::new(url).unwrap();
-                let image = code.render::<Luma<u8>>().min_dimensions(512, 512).quiet_zone(false).build();
-                let mut output = Vec::new();
-                let encoder = PngEncoder::new(&mut output);
+
+                // Render the QR code as a bitmap
+                let image = code
+                    .render::<Luma<u8>>()
+                    .min_dimensions(512, 512)
+                    .quiet_zone(false)
+                    .build();
+
+                // Send the response headers to the client and get a writable stream
+                let resp = Response::from_status(StatusCode::OK).with_content_type(mime::IMAGE_PNG);
+                let body = resp.stream_to_client();
+
+                // Encode the rendered image as a PNG and stream it to the client
+                let encoder = PngEncoder::new(body);
                 let (width, height) = image.dimensions();
                 encoder.encode(&image.into_raw(), width, height, ColorType::L8)?;
-                Ok(Response::from_body(output).with_content_type(mime::IMAGE_PNG))
             } else {
-                Ok(Response::from_status(StatusCode::BAD_REQUEST).with_body_text_plain("No URL provided"))
+                Response::from_status(StatusCode::BAD_REQUEST)
+                    .with_body_text_plain("No URL provided")
+                    .send_to_client();
             }
-        },
-        _  => {
-            Ok(Response::from_status(StatusCode::NOT_FOUND))
         }
+        _ => {
+            Response::from_status(StatusCode::NOT_FOUND).send_to_client();
+        }
+    }
+
+    Ok(())
+}
+
+// Cannot use #[fastly::main] here because we need to send a streaming body
+fn main() {
+    let req = Request::from_client();
+    if let Err(err) = handle_request(req) {
+        Response::from_status(StatusCode::INTERNAL_SERVER_ERROR).send_to_client();
+        panic!("{:?}", err);
     }
 }
